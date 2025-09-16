bbb24452c9458a486f0dbb586b63a7418c014b32
anushka-dm <anushkadm.dev@gmail.com>
2025-09-16 15:13:38 +0530

    2.Fraction Class

---
+++ oops/Fraction.java
@@ -0,0 +1,69 @@
+public class Fraction {
+
+	public int numerator;
+	public int denominator;
+
+	public Fraction(int numerator, int denominator) {
+
+		if (denominator <= 0) {
+			return;
+		}
+		this.numerator = numerator;
+		this.denominator = denominator;
+		simpliyfy();
+	}
+
+	public void simpliyfy() {
+		int gdc = 1;
+		int smaller = Math.min(numerator, denominator);
+
+		for (int i = 2; i <= smaller; i++) {
+			if (numerator % i == 0 && denominator % i == 0) {
+				gdc = i;
+			}
+		}
+
+		numerator = numerator / gdc;
+		denominator = denominator / gdc;
+	}
+
+	public void increment() {
+		numerator = numerator + denominator;
+		simpliyfy();
+	}
+
+	public void print() {
+		System.out.println(numerator + "/" + denominator);
+	}
+
+	public int getNumerator() {
+		return numerator;
+	}
+
+	public int getDenominator() {
+		return denominator;
+	}
+
+	public void setNumberator(int num) {
+		this.numerator = num;
+		simpliyfy();
+	}
+
+	public void setDenominatior(int num) {
+		if (num <= 0) {
+			return;
+			// need to thow error
+		}
+		this.denominator = num;
+		simpliyfy();
+	}
+
+	// add 2 franctions
+
+	public void add(Fraction f2) {
+		this.numerator = this.numerator * f2.denominator + this.denominator * f2.numerator;
+		this.denominator = this.denominator * f2.denominator;
+		simpliyfy();
+	}
+
+}
---
+++ oops/FractionClass.java
@@ -0,0 +1,13 @@
+public class FractionClass {
+
+	public static void main(String[] args) {
+		Fraction f1 = new Fraction(4, 6);
+		f1.print();
+		// f1.increment();
+
+		Fraction f2 = new Fraction(4, 8);
+		f2.print();
+		f1.add(f2);
+		f1.print();
+	}
+}
